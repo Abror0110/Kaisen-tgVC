5071adc0cdc895ca71f73aa5586afd167c1b8594
Zhend X <86902404+Abror0110@users.noreply.github.com>
2021-10-07 20:52:54 +0700

    Update kaisen.py

--- kaisen.py
+++ kaisen.py
@@ -192,6 +192,74 @@ async def leave_voice_chat(client, message):
     VOICE_CHATS.pop(chat_id, None)
     await message.reply('Meninggalkan Voice Chat ✅')
 
+
+@app.on_message(filters.command('videoplay') & self_or_contact_filter)
+async def video_voice_chat(Clint, message):
+    xx = await eor(event, get_string("com_1"))
+    chat = event.chat_id
+    from_user = inline_mention(event.sender)
+    reply, song = None, None
+    if event.reply_to:
+        reply = await event.get_reply_message()
+    if len(event.text.split()) > 1:
+        input = event.text.split(maxsplit=1)[1]
+        tiny_input = input.split()[0]
+        if tiny_input.startswith("@"):
+            try:
+                chat = int("-100" + str(await get_user_id(tiny_input, client=vcClient)))
+                song = input.split(maxsplit=1)[1]
+            except IndexError:
+                pass
+            except Exception as e:
+                return await eor(event, str(e))
+        elif tiny_input.startswith("-"):
+            chat = int(
+                "-100" + str(await get_user_id(int(tiny_input), client=vcClient))
+            )
+            try:
+                song = input.split(maxsplit=1)[1]
+            except BaseException:
+                pass
+        else:
+            song = input
+    if not (reply or song):
+        return await eor(
+            xx, "Please specify a song name or reply to a video file !", time=5
+        )
+    await eor(xx, "`Downloading and converting...`")
+    if reply and reply.media and mediainfo(reply.media).startswith("video"):
+        song, thumb, title, link, duration = await file_download(xx, reply)
+    else:
+        is_link = is_url_ok(song)
+        if is_link is False:
+            return await eor(xx, f"`{song}`\n\nNot a playable link.🥱")
+        if is_link is None:
+            song, thumb, title, link, duration = await vid_download(song)
+        elif re.search("youtube", song) or re.search("youtu", song):
+            song, thumb, title, link, duration = await vid_download(song)
+        else:
+            song, thumb, title, link, duration = (
+                song,
+                "https://telegra.ph/file/22bb2349da20c7524e4db.mp4",
+                song,
+                song,
+                "♾",
+            )
+    ultSongs = Player(chat, xx, True)
+    if not (await ultSongs.vc_joiner()):
+        return
+    await xx.reply(
+        "🎸 **Now playing:** [{}]({})\n⏰ **Duration:** `{}`\n👥 **Chat:** `{}`\n🙋‍♂ **Requested by:** {}".format(
+            title, link, duration, chat, from_user
+        ),
+        file=thumb,
+        link_preview=False,
+    )
+    await asyncio.sleep(1)
+    await ultSongs.group_call.start_video(song, with_audio=True)
+    await xx.delete()
+
+
 app.start()
 print('>>> Userbot Dimulai')
 idle()
