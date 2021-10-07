be998c372ebeb64a2936b1f392a23035e0eb3305
Zhend X <86902404+Abror0110@users.noreply.github.com>
2021-10-08 00:01:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -194,11 +194,11 @@ async def leave_voice_chat(client, message):
 
 @app.on_message(filters.command('stream') & self_or_contact_filter)
 async def stream_vc_chat(client, message):
-    global vc_live
+    global stream_vc
     if not message.reply_to_message or not message.reply_to_message.audio:
         return
     msg = await message.reply("⏳ __Please wait.__")
-    if vc_live == True:
+    if stream_vc == True:
         return await msg.edit("💬 __Live or Radio Ongoing. Please stop it via `!endvc`.__")
     media = message.reply_to_message
     THUMB_URL, VIDEO_TITLE, VIDEO_DURATION = "https://appletld.com/wp-content/uploads/2020/10/E3593D8D-6F1C-4A16-B065-2154ED6B2355.png", "Music", "Not Found"
